351cf9449e974dd68c724368562409c09d1ec307
Golden <cute.golden.protogen.2@gmail.com>
2025-07-30 17:15:18 -0400

    Delete ScoreboardTweaks directory

--- ScoreboardTweaks/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ModConstants
-{
-    class ModConstants
-    {
-        public const string modGUID = "net.rusjj.scoreboardtweaks";
-        public const string modName = "Scoreboard Tweaks";
-        public const string modVersion = "1.1.0";
-    }
-}
